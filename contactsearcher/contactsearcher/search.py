6e9b17dd657086f4a273276c0dba22beecbdefa2
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-09-25 09:25:26 -0400

    Add the correct TODO markers into the search file.

---
+++ contactsearcher/contactsearcher/search.py
@@ -0,0 +1,23 @@
+"""Search for an email address given a fragment of a job description."""
+
+import csv
+
+
+def search_for_email_given_job(job_description: str, contacts: str):
+    """Search for and return job(s) given an email address."""
+    # create an empty list of the contacts
+    contacts_list = []
+    # --> refer to the file called inputs/contacts.txt to learn more about
+    # the format of the comma separated value (CSV) file that we must parse
+    # --> iterate through each line of the file and extract the current job
+    for contact_line in csv.reader(
+        contacts.splitlines(),
+        quotechar='"',
+        delimiter=",",
+        quoting=csv.QUOTE_ALL,
+        skipinitialspace=True,
+    ):
+        # TODO: extract the current job for the contact on this line of the CSV
+        # TODO: the job description matches and thus we should save it in the list
+    # return the list of the contacts who have a job description that matches
+    return contacts_list
